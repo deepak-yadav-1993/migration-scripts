6ef680c15f4038282e25db163e5611f4fa61ad46
Deepak Yadav <deepak.yadav@ericsson.com>
2020-03-11 11:49:12 -0400

    Synced lated configs

--- copy-current-configs.sh
+++ copy-current-configs.sh
@@ -4,4 +4,4 @@ echo "copying zshrc.."
 cp ~/.zshrc ./zshrc.txt
 
 echo "Please copy the latest json profile into this directory from\n iterm2>preferences>profiles>Other Actions>Save all profiles as json\n"
-echo "run ./import-and-install-configs to import the configs"
+echo "run ./import-and-install-configs to import the configs\n"
