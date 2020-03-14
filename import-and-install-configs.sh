9fa333fbecd264c5421e20f2d94b3afe0d366b2b
Deepak Yadav <deepak.yadav@ericsson.com>
2020-03-13 20:08:06 -0400

    Pretified import script

--- import-and-install-configs.sh
+++ import-and-install-configs.sh
@@ -1,7 +1,7 @@
 echo "Install NVM?\n 1. Yes 2. No\n"
 read nvm_confirmation
 if [ $nvm_confirmation -eq 1 ]
-    then
+then
     curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
 else
     echo "Skipping NVM install\n"
@@ -10,11 +10,11 @@ fi
 echo "Download cenx code and install dependencies?\n 1. Yes 2. No\n"
 read cenx_confirmation
 if [ $cenx_confirmation -eq 1 ]
-    then
+then
     echo "Downloading cenx codebase....."
     cd ~ && mkdir ~/Ericsson/code && cd ~/Ericsson/code
     git clone git@git.rosetta.ericssondevops.com:cenx/cenx.git
-
+    
     echo "Installing cenx dependencies..."
     cd ~/Ericsson/code/cenx/apps/frontend/ && nvm install && npm run bootstrap
 else
@@ -27,20 +27,20 @@ read packages
 brew install $packages
 
 echo "Brewing basic graphic based applications\n visual-studio-code \n docker \n spotify \n graphql-ide \n iterm2 \n firefox \n google-chrome \n microsoft-teams\n"
-brew cask install \\ 
-visual-studio-code \ 
-docker \ 
-spotify \ 
-graphql-ide \ 
-iterm2 \ 
-firefox \ 
-google-chrome \ 
+brew cask install \\
+visual-studio-code \
+docker \
+spotify \
+graphql-ide \
+iterm2 \
+firefox \
+google-chrome \
 microsoft-teams
 
 echo "Install other brew_cask graphic packages?\n 1. Yes\n 2. No\n"
 read brew_cask_packages_confirmation
 if [ $brew_cask_packages_confirmation -eq 1 ]
-    then
+then
     echo "Enter the packages to install separated by space \nExample: docker iterm2 firefox\n"
     read cask_packages
     brew cask install $cask_packages
