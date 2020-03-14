4dbde4f53547e78de0fcbce97c7aa73338792c49
Deepak Yadav <deepak.yadav@ericsson.com>
2020-03-13 20:36:54 -0400

    Updated permissions of all files

--- copy-current-configs.sh
+++ copy-current-configs.sh
@@ -1,7 +1,7 @@
 echo "copying vs-code extensions..."
 code --list-extensions  > vscode-extensions.txt
 echo "copying zshrc.."
-cp ~/.zshrc ./zshrc.txt
+cp ~/.zshrc ./zshrc.sh
 
 echo "Please copy the latest json profile into this directory from\n iterm2>preferences>profiles>Other Actions>Save all profiles as json\n"
 echo "run ./import-and-install-configs to import the configs\n"
