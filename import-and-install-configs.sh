0078fabc6784f1b6aa05783351e56d1faf57d8bc
Deepak Yadav <deepakYadav_personalMacBook>
2021-11-15 21:31:33 -0500

    Changing zsh theme

--- import-and-install-configs.sh
+++ import-and-install-configs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-import_configs(){
+import_configs() {
     # echo "Install NVM?\n 1. (Yes)\n 2. (No)\n"
     # read nvm_confirmation
     # if [ $nvm_confirmation -eq 1 ]
@@ -8,18 +8,18 @@ import_configs(){
     # else
     #     echo "Skipping NVM install\n"
     # fi
-    
+
     # Check to see if Homebrew is installed, and install it if it is not
     command -v brew >/dev/null 2>&1 || {
         echo >&2 "Installing Homebrew Now"
         /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     }
-    
+
     echo "Brewing basic graphic based applications\n visual-studio-code \n docker \n spotify \n graphql-ide \n iterm2 \n
     firefox \n google-chrome \n postman\n drawio\n"
     brew update
     brew install --cask visual-studio-code docker spotify graphql-ide iterm2 firefox google-chrome postman drawio
-    
+
     echo "\nInstall other brew_cask graphic packages?\n 1. (Yes)\n 2. (No)\n"
     read brew_cask_packages_confirmation
     if [ $brew_cask_packages_confirmation -eq 1 ]; then
@@ -29,11 +29,11 @@ import_configs(){
     else
         echo "Skipping more brew_cask installs\n"
     fi
-    
+
     echo "Brewing basic development packages \n git zsh nvm zsh-autosuggestions \n"
     brew install git zsh nvm zsh-autosuggestions zsh-syntax-highlighting
     echo "\n Make sure you have nvm home environment variable set up\n export NVM_DIR="$HOME/.nvm""
-    
+
     echo "\nInstall other brew packages?\n 1. (Yes)\n 2. (No)\n"
     read brew_packages_confirmation
     if [ $brew_packages_confirmation -eq 1 ]; then
@@ -43,43 +43,44 @@ import_configs(){
     else
         echo "Skipping more brew installs\n"
     fi
-    
+
     echo "\nWill append ~/.zshrc with the contents of file .zshrc.txt ! Continue?\n 1. (Yes)\n 2. (No)\n"
     read zshrc_import_confirm
     if [ $zshrc_import_confirm -eq 1 ]; then
         echo "Appending at the end of .zshrc file"
         # rm ~/.zshrc
-        cat zshrc.txt >> ~/.zshrc
+        cat zshrc.txt >>~/.zshrc
     else
         echo "Skipping .zshrc import\n"
     fi
-    
+
     echo "\nInstalling vscode extensions.."
     while read line; do
         code --install-extension $line
     done <vscode-extensions.txt
-    
+
     echo "\nMoving settings.json.."
     cp settings.json ~/Library/Application\ Support/Code/User/settings.json
-    
-    echo "\n Installing powerline font\n"
-    {
-        # clone
-        git clone https://github.com/powerline/fonts.git --depth=1
-        # install
-        cd fonts
-        ./install.sh
-        # clean-up a bit
-        cd ..
-        rm -rf fonts
-    } &>/dev/null # For no output. Remove &> /dev/null for debugging
-    
-    open https://github.com/powerline/fonts
-    
-    echo "\ue0b0 \u00b1 \ue0a0 \u27a6 \u2718 \u26a1 \u2699"
-    
+
+    # This to be used only when agnoster theme is being used for zsh
+    # echo "\n Installing powerline font\n"
+    # {
+    #     # clone
+    #     git clone https://github.com/powerline/fonts.git --depth=1
+    #     # install
+    #     cd fonts
+    #     ./install.sh
+    #     # clean-up a bit
+    #     cd ..
+    #     rm -rf fonts
+    # } &>/dev/null # For no output. Remove &> /dev/null for debugging
+
+    # open https://github.com/powerline/fonts
+
+    # echo "\ue0b0 \u00b1 \ue0a0 \u27a6 \u2718 \u26a1 \u2699"
+
     echo "\n To make sure terminal font works okay, import ./Profiles.json or Deepak.json in \n iterm2>preferences>profiles>Other Actions>Import JSON Profiles..."
-    
+
 }
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
