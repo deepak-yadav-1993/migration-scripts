088c4f69860c842e28ab24e3246d66a26d92fb92
Deepak Yadav <deepak.yadav@lightspeedhq.com>
2022-11-14 10:09:02 +0530

    Updated import script

--- import-and-install-configs.sh
+++ import-and-install-configs.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+invalid_response="Invalid Response\n\n"
+
 import_configs() {
     # echo "Install NVM?\n 1. (Yes)\n 2. (No)\n"
     # read nvm_confirmation
@@ -15,51 +18,68 @@ import_configs() {
         /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     }
 
-    echo "Brewing basic graphic based applications\n visual-studio-code \n docker \n spotify \n graphql-ide \n iterm2 \n
+    printf "Brewing basic graphic based applications\n visual-studio-code \n docker \n spotify \n graphql-ide \n iterm2 \n
     firefox \n google-chrome \n postman\n drawio\n"
     brew update
     brew install --cask visual-studio-code docker spotify graphql-ide iterm2 firefox google-chrome postman drawio
 
-    echo "\nInstall other brew_cask graphic packages?\n 1. (Yes)\n 2. (No)\n"
-    read brew_cask_packages_confirmation
-    if [ $brew_cask_packages_confirmation -eq 1 ]; then
-        echo "Enter the packages to install separated by space \nExample: docker iterm2 firefox\n"
+    read -p "\nInstall other brew_cask graphic packages? (y/n) `echo $'\n> '`" yn
+    case $yn in
+    [yY] | yes | Yes | YES)
+        printf "Enter the packages to install separated by space \nExample: docker iterm2 firefox\n"
         read cask_packages
         brew cask install $cask_packages
-    else
-        echo "Skipping more brew_cask installs\n"
-    fi
+        ;;
+    [nN] | no | No | NO)
+        printf "Skipping more brew_cask installs\n\n"
+        ;;
+    *)
+        printf $invalid_response
+        ;;
+    esac
 
-    echo "Brewing basic development packages \n git zsh nvm zsh-autosuggestions \n"
+
+    printf "Brewing basic development packages \n git zsh nvm zsh-autosuggestions \n"
     brew install git zsh nvm zsh-autosuggestions zsh-syntax-highlighting
-    echo "\n Make sure you have nvm home environment variable set up\n export NVM_DIR="$HOME/.nvm""
+    printf "\n Make sure you have nvm home environment variable set up\n export NVM_DIR="$HOME/.nvm""
 
-    echo "\nInstall other brew packages?\n 1. (Yes)\n 2. (No)\n"
-    read brew_packages_confirmation
-    if [ $brew_packages_confirmation -eq 1 ]; then
-        echo "Enter the packages to install separated by space \nExample: git nvm helm\n"
+
+    read -p "Install other brew packages? (y/n) `echo $'\n> '`" yn
+    case $yn in
+    [yY] | yes | Yes | YES)
+        printf "Enter the packages to install separated by space \nExample: git nvm helm\n"
         read brew_packages
         brew install $brew_packages
-    else
-        echo "Skipping more brew installs\n"
-    fi
+        ;;
+    [nN] | no | No | NO)
+        printf "Skipping more brew installs\n\n"
+        ;;
+    *)
+        printf $invalid_response
+        ;;
+    esac
 
-    echo "\nWill append ~/.zshrc with the contents of file .zshrc.txt ! Continue?\n 1. (Yes)\n 2. (No)\n"
-    read zshrc_import_confirm
-    if [ $zshrc_import_confirm -eq 1 ]; then
+    read -p "Will append ~/.zshrc with the contents of file .zshrc.txt ! Continue? (y/n) `echo $'\n> '`" yn
+    case $yn in
+    [yY] | yes | Yes | YES)
         echo "Appending at the end of .zshrc file"
         # rm ~/.zshrc
         cat zshrc.txt >>~/.zshrc
-    else
-        echo "Skipping .zshrc import\n"
-    fi
+        ;;
+    [nN] | no | No | NO)
+        printf "Skipping .zshrc import\n\n"
+        ;;
+    *)
+        printf $invalid_response
+        ;;
+    esac
 
-    echo "\nInstalling vscode extensions.."
+    printf "\nInstalling vscode extensions.."
     while read line; do
         code --install-extension $line
     done <vscode-extensions.txt
 
-    echo "\nMoving settings.json.."
+    printf "\nMoving settings.json.."
     cp settings.json ~/Library/Application\ Support/Code/User/settings.json
 
     # This to be used only when agnoster theme is being used for zsh
